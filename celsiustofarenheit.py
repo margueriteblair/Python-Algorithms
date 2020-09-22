72b49dc2af74e1b02f474a9c69899eecfdff0c24
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-22 18:36:31 -0400

    factorialize with python cli

--- celsiustofarenheit.py
+++ celsiustofarenheit.py
@@ -3,3 +3,7 @@ def convert(celsius):
     print(far)
 
 convert(-30)
+
+def convert2(celsius):
+    far = celsius*(9/5)+32
+    print(far)
--- factorialize.py
+++ factorialize.py
@@ -1,7 +1,15 @@
-def factorial(num):
+def factorialize(num):
     total = 1
     for i in range(1, num+1):
-        total *= i
-    return(total)
+        total = total * i
+    print(total)
 
-factorial(5)
+factorialize(5)
+
+def factorialize2():
+    q = input('What number would you like to factorialize? ')
+    total = 1
+    for i in range(1, int(q)+1):
+        total = total * i
+    print(total)
+factorialize2()
