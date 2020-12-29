0a704a7e2ba0b7200e8f37205d1841cae5ac6a84
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-29 02:02:26 -0500

    minor upgrade using built in dict method

--- Dictionary.py
+++ Dictionary.py
@@ -19,8 +19,7 @@ while True:
 
 
 n = int(input())
-name_numbers = [input().split() for _ in range(n)]
-phone_book = {k: v for k,v in name_numbers}
+phone_book = dict(input().split() for _ in range(n))
 while True:
     try:
         name = input()
