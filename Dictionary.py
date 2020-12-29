4d700d44299de323a0172321de8b6b1276035c91
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-29 02:08:16 -0500

    removed old method

--- Dictionary.py
+++ Dictionary.py
@@ -1,23 +1,3 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-N = int(input())
-arr_map = []
-for i in range(N):
-    x = input().split()
-    y = dict(name= x[0], phoneNumber=x[1])
-    arr_map.append(y)
-while True:
-    try:
-        inp = input()
-        if (inp == ""):
-            break
-        for dictionary in arr_map:
-            if (dictionary["name"] == inp):
-                print(inp + "=" +dictionary["phoneNumber"])
-        
-    except EOFError as e:
-        break
-
-
 n = int(input())
 phone_book = dict(input().split() for i in range(n))
 while True:
