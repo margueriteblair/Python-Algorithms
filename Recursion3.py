2fa13cb436d69be13c605e24e626016df1614986
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-29 15:21:45 -0500

    recusion to find fibonacci sequence

---
+++ Recursion3.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    sumF = 1
+    for i in range(2, n+1):
+        sumF *= i
+    return sumF;
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input())
+    result = factorial(n)
+    fptr.write(str(result) + '\n')
+    fptr.close()
