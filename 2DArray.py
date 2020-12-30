de90f805414c48869e2e5d9c293c6803a007c654
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-30 16:43:03 -0500

    fixed it for ccases where the max sum will be a negative number

--- 2DArray.py
+++ 2DArray.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
-    maxHourglass = 0
+    maxHourglass = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2]
     for i in range(0, len(arr)-2):
         for j in range(0, len(arr[i])-2):
             curr = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
